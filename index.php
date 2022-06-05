19b2f8787db827912fa31368e12eeae50b159d4e
Samuel <samuel.sluga@gmail.com>
2022-06-05 12:18:54 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include './head.html';?>
+<?php include './navigation.html';?>
+<?php include './header.html';?>
+
+<article>
+</article>
+
+<?php include './footer.html';?>
+</body>
+</html>
