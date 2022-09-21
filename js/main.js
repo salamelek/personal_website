58d827910bbbc9e2b6a55281b8aae1b37be6d73a
Samuel <samuel.sluga@gmail.com>
2022-09-21 22:39:27 +0200

    added dark mode, project card work, curriculum update text

--- js/main.js
+++ js/main.js
@@ -1,12 +1,43 @@
-function copyText(ele) {
-    let contact = document.getElementById(ele.id);
+function copyText(e) {
+    let contact = document.getElementById(e.id);
 
     navigator.clipboard.writeText(contact.innerText);
 
     contact.classList.add("copied")
 }
 
-function resetTooltip(ele) {
-    let contact = document.getElementById(ele.id);
+function resetTooltip(e) {
+    let contact = document.getElementById(e.id);
     contact.classList.remove("copied")
+}
+
+function setTheme(theme) {
+    document.documentElement.setAttribute('data-theme', theme);
+    window.localStorage.setItem("currentTheme", theme);
+}
+
+function checkTheme() {
+    if (window.localStorage.getItem("currentTheme") == null) {
+        if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+            // dark mode
+            setTheme("dark")
+        } else {
+            // light mode
+            setTheme("light")
+        }
+    } else {
+        if (window.localStorage.getItem("currentTheme") === "dark") {
+            setTheme("dark")
+        } else {
+            setTheme("light")
+        }
+    }
+}
+
+function switchTheme() {
+    if (window.localStorage.getItem("currentTheme") !== "dark") {
+        setTheme("dark")
+    } else {
+        setTheme("light")
+    }
 }
