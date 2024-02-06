e677648d7fc723858347608c2f3134eef20d1691
salamelek <samuel.sluga@gmail.com>
2024-02-06 18:05:01 +0100

    added discord contact, have to do the copy button animation

--- js/main.js
+++ js/main.js
@@ -20,3 +20,33 @@ function switchTheme() {
         setTheme("light")
     }
 }
+
+function positiveClick(element, duration=500) {
+    const animationElement = document.getElementById("animation-div");
+
+    const xy = getPositionRelativeToPage(element);
+    const rect = element.getBoundingClientRect();
+
+    let x = xy.left + (rect.width / 2);
+    let y = xy.top + (rect.height / 2);
+
+    animationElement.style.top = y.toString() + "px";
+    animationElement.style.left = x.toString() + "px";
+    animationElement.style.transform = "scale(1)";
+    animationElement.style.transform = "translateX(-50%) translateY(-50%)";
+
+    setTimeout(() => {
+        animationElement.style.transform = "scale(0)";
+    }, duration);
+}
+
+function getPositionRelativeToPage(element) {
+    const rect = element.getBoundingClientRect();
+    const scrollTop = window.scrollY || window.pageYOffset;
+    const scrollLeft = window.scrollX || window.pageXOffset;
+
+    const top = rect.top + scrollTop;
+    const left = rect.left + scrollLeft;
+
+    return {top, left};
+}
